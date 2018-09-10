70c7373063e4294f200e68b422e724c79c82ee4b
Benjamin <bhillenbrand101@gmail.com>
2018-09-10 13:19:00 -0500

    Basic game concept

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 
-print("hello world")
+import random
+import CardDeck
+
+
+arrayPlayer1 = []
+arrayPlayer2 = []
+
+
+for x in range(3):
+    rando_card = random.choice(CardDeck.cardDictiony)
+    arrayPlayer1.append(rando_card)
+
+print(arrayPlayer1)
+
+
+
