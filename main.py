13a2ab8f96ce88665ff16fd8a2f88805396a66fa
Benjamin <bhillenbrand101@gmail.com>
2018-09-10 13:49:27 -0500

    Cards now "pop"

--- main.py
+++ main.py
@@ -3,15 +3,31 @@ import random
 import CardDeck
 
 
+def draw_cards():  # todo "pop" cards from deck when pulled
+    rande_number = random.randint(0, 52)
+    rando_card = (CardDeck.cardDictiony.pop(rande_number))
+    #CardDeck.cardDictiony.pop(rando_card)
+    return rando_card
+
+
 arrayPlayer1 = []
 arrayPlayer2 = []
+arrayPlayer3 = []
+arrayPlayer4 = []
+
 
 
 for x in range(3):
-    rando_card = random.choice(CardDeck.cardDictiony)
-    arrayPlayer1.append(rando_card)
+    card = draw_cards()
+    arrayPlayer1.append(card)
 
+
+
+
+print("Your cards are: ")
 print(arrayPlayer1)
+#print("Player two cards: ")
+#print(arrayPlayer2)
 
 
 
